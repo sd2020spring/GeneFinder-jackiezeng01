9e3f9a2c690566ef818f1903a3d51922a1efbb0a
Jackie <dolphin.pool01@gmail.com>
2020-02-11 00:20:23 -0500

    updated unit testing for functions

--- gene_finder.py
+++ gene_finder.py
@@ -27,6 +27,8 @@ def get_complement(nucleotide):
     'T'
     >>> get_complement('C')
     'G'
+    >>> get_complement('G')
+    'C'
     """
     if(nucleotide == 'A'):
         return 'T'
@@ -47,6 +49,8 @@ def get_reverse_complement(dna):
     'AAAGCGGGCAT'
     >>> get_reverse_complement("CCGCGTTCA")
     'TGAACGCGG'
+    >>> get_reverse_complement("CTGATGATG")
+    'CATCATCAG'
     """
     r_comp = ''
     for i in dna:
@@ -66,6 +70,9 @@ def rest_of_ORF(dna):
     'ATG'
     >>> rest_of_ORF("ATGTAG")
     'ATG'
+    >>> rest_of_ORF("ATGGCC")
+    'ATGGCC'
+
     """
     stop1 = 'TAG'
     stop2 = 'TAA'
@@ -93,6 +100,8 @@ def find_all_ORFs_oneframe(dna):
     ['ATG']
     >>> find_all_ORFs_oneframe("ATGCGAATGTAGCATCAAA")
     ['ATGCGAATG']
+    >>> find_all_ORFs_oneframe("ATGCATGAATGTAG")
+    ['ATGCATGAATGTAG']
 
     """
     ORF_array = []
@@ -143,6 +152,8 @@ def find_all_ORFs_both_strands(dna):
         returns: a list of non-nested ORFs
     >>> find_all_ORFs_both_strands("ATGCGAATGTAGCATCAAA")
     ['ATGCGAATG', 'ATGCTACATTCGCAT']
+    >>> find_all_ORFs_both_strands("ATGCATGAATGTAG")
+    ['ATGCATGAATGTAG', 'ATGAATGTAG', 'ATG', 'ATGCAT']
     """
     all_ORF_both_strand = []
     ORF1 = find_all_ORFs(dna)
@@ -157,6 +168,8 @@ def longest_ORF(dna):
         as a string
     >>> longest_ORF("ATGCGAATGTAGCATCAAA")
     'ATGCTACATTCGCAT'
+    >>> longest_ORF("ATGCATGAATGTAG")
+    'ATGCATGAATGTAG'
     """
     all_ORFs = find_all_ORFs_both_strands(dna)
     longest_ORF = max(all_ORFs, key=len)
@@ -174,8 +187,8 @@ def longest_ORF_noncoding(dna, num_trials):
     ORFs_arr = []
     while (i > 0):
          shuffled_dna = shuffle_string(dna)
-         longest_ORF = longest_ORF(shuffled_dna)
-         ORFs_arr.append(longest_ORF)
+         ORF = longest_ORF(shuffled_dna)
+         ORFs_arr.append(ORF)
          i = i-1
     overall_longest_ORF = max(ORFs_arr, key=len)
     return len(overall_longest_ORF)
@@ -194,6 +207,8 @@ def coding_strand_to_AA(dna):
         'MR'
         >>> coding_strand_to_AA("ATGCCCGCTTT")
         'MPA'
+        >>> coding_strand_to_AA("ATGTTACAGCGT")
+        'MLQ'
     """
     aa_str = ''
     i = 0
@@ -210,7 +225,6 @@ def gene_finder(dna):
         dna: a DNA sequence
         returns: a list of all amino acid sequences coded by the sequence dna.
     """
-    # TODO: implement this
     aa_arr = []
     threshold = longest_ORF_noncoding(dna, 1500)
     both_strands_ORFs = find_all_ORFs_both_strands(dna)
@@ -222,11 +236,10 @@ def gene_finder(dna):
 
 
 if __name__ == "__main__":
-    # import doctest
-    #
-    # dna = load_seq("./data/X73525.fa")
     from load import load_seq
     dna = load_seq("./data/X73525.fa")
     print(gene_finder(dna))
+
+    # import doctest
     # doctest.testmod()
     # doctest.run_docstring_examples(coding_strand_to_AA, globals(), verbose=True)
